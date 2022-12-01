6ebd8a18b96f3c00ac3d4d6a946e811eeb1a9490
Lunaticfrg0 <emilio3scano@gmail.com>
2022-11-30 22:07:28 -0400

    set nullable the addreses and numbers

--- Backend/ManagmentTest/Business.Mappers/Dto/ClientDto.cs
+++ Backend/ManagmentTest/Business.Mappers/Dto/ClientDto.cs
@@ -5,8 +5,8 @@
         public Guid Id { get; set; }
         public string Name { get; set; }
         public string Lastname { get; set; }
-        public IEnumerable<ClientAddressDto> ClientAddresses { get; set; }
-        public IEnumerable<ClientNumberDto> ClientNumbers { get; set; }
+        public IEnumerable<ClientAddressDto>? ClientAddresses { get; set; }
+        public IEnumerable<ClientNumberDto>? ClientNumbers { get; set; }
         public DateTime BirthDate { get; set; }
     }
 }
